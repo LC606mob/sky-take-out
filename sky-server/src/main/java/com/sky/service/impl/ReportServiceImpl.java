d3e621b79e899029570fbdc8ead8605bd5a3cc6f
LC606mob <2083446477@qq.com>
2026-01-10 20:04:25 +0800

    用户统计业务功能开发

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import org.apache.commons.lang.StringUtils;
 import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,8 @@ import java.util.Map;
 public class ReportServiceImpl implements ReportService {
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private UserMapper userMapper;
     /**
      * 统计指定时间区间内的营业额
      * @param begin
@@ -66,6 +70,53 @@ public class ReportServiceImpl implements ReportService {
         return TurnoverReportVO.builder()
                 .dateList(StringUtils.join(dateList,","))
                 .turnoverList(StringUtils.join(turnoverList,","))
+                .build();
+    }
+
+    /**
+     * 用户数据统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //业务逻辑：
+        //核心逻辑在于循环计算每一天的新增用户和总人数：
+        //1. 计算日期轴：从 begin 到 end 构造日期列表 dateList。
+        //2. 循环统计数量：
+        //    ◦ 遍历日期列表，计算每日的起始时间（00:00:00）和结束时间（23:59:59）。
+        //    ◦ 查询新增用户：调用 userMapper.countByMap，传入当天的起始和结束时间。
+        //    ◦ 查询总用户量：调用 userMapper.countByMap，仅传入当天的结束时间作为上限。
+        //3. 数据拼接：使用 StringUtils.join 将集合转换为逗号分隔的字符串
+
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while (!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        //存放每天发总用户数select count(id) from user where create_time < ?
+        List<Integer> totalUserList = new ArrayList<>();
+        //存放每天的新增用户数select count(id) from user where create_time < ? and creat_time > ?
+        List<Integer> newUserList = new ArrayList<>();
+
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date,LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date,LocalTime.MAX);
+            Map map = new HashMap<>();
+            map.put("end",endTime);//总用户数量 select count(id) from user where  create_time < ?
+            Integer totalUser = userMapper.countByMap(map);
+            map.put("begin",beginTime);
+            //新增用户数量 select count(id) from user where create_time > ? and create_time < ?
+            Integer newUser = userMapper.countByMap(map);
+            totalUserList.add(totalUser);
+            newUserList.add(newUser);
+        }
+
+        return UserReportVO.builder()
+                .dateList(StringUtils.join(dateList,","))
+                .newUserList(StringUtils.join(newUserList,","))
+                .totalUserList(StringUtils.join(totalUserList,","))
                 .build();
     }
 }
