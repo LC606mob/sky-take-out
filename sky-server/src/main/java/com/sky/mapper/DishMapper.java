2bb2f73ba708475f61a76064498ddfadfbe330ac
LC606mob <2083446477@qq.com>
2026-01-11 14:46:37 +0800

    工作台业务功能开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -53,4 +54,10 @@ public interface DishMapper {
     List<Dish> list(Dish dish);
 
     List<Integer> getDishStatusBySetmealId(Long id);
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
